da6c0780ad97f5222ce0153a8b30e0dda650b122
lasting <lastingcoder@qq.com>
2022-05-26 17:01:55 +0800

    add more new test

--- src/pages/RefDemo/index.tsx
+++ src/pages/RefDemo/index.tsx
@@ -1,24 +1,29 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDebouncing } from '../../utils/deBoucing'
 import { useThrotting } from '../../utils/throtting'
 
 export default function RefDemo() {
-    const inputText=useRef<HTMLInputElement>(null)
-    const [value,setValue]=useState({value:1})
-    const deBouncingValue=useThrotting(value,2000,2000)
+  const inputText = useRef<HTMLInputElement>(null)
+  const [value, setValue] = useState({ value: 1 })
+  const deBouncingValue = useThrotting(value, 2000, 2000)
 
-    console.log(null==undefined)
+  useEffect(()=>{
+    console.log('1')
+    return ()=>console.log('2')
+  })
+
+  console.log(null == undefined)
   return (
-    <div style={{width:'200px',margin:'10px auto'}}>
-        <input ref={inputText} type="text" />
+    <div style={{ width: '200px', margin: '10px auto' }}>     
+        <input name='test' ref={inputText} type="text" />
         <div>{value.value}</div>
         <div>{deBouncingValue.value}</div>
-        <button onClick={()=>{
-            if(inputText.current===null) return
-            inputText.current.focus()
-            setValue((i)=>{
-              return {value:i.value+1}
-            })
+        <button onClick={() => {
+          if (inputText.current === null) return
+          inputText.current.focus()
+          setValue((i) => {
+            return { value: i.value + 1 }
+          })
         }}>focus</button>
     </div>
   )
