777ecfeab3f302b2e896527d81f0b04959248446
lasting <lastingcoder@qq.com>
2022-06-14 09:31:19 +0800

    完成hidden search

--- src/pages/Product/hidden-search/index.tsx
+++ src/pages/Product/hidden-search/index.tsx
@@ -10,7 +10,7 @@ export default function HiddenSearch() {
         const input = document.querySelector('.input') as HTMLInputElement
 
         btn.addEventListener('click', () => {
-            search.classList.toggle('active')
+            search.classList.toggle('show')
             input.focus()
         })
     }, [])
@@ -18,7 +18,7 @@ export default function HiddenSearch() {
     return (
         <div id='hiddenSearch'>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossOrigin="anonymous" />
-            <div className="search">
+            <div className="search show">
                 <input type="text" className="input" placeholder="Search..." />
                 <button className="btn">
                     <i className="fas fa-search"></i>
