2e23d08c0e551894f59246a1a40aa43d7f5ccfa0
lasting <lastingcoder@qq.com>
2022-06-23 16:13:28 +0800

    * 修改gitignore * 新增三列布局

--- src/components/Products/css-layout-practice/index.tsx
+++ src/components/Products/css-layout-practice/index.tsx
@@ -6,7 +6,7 @@ export default function CssLayoutPractice() {
     return (
         <div className='cssLayoutPractice'>
 
-            <h2>css九宫格布局flex实现</h2>
+            <h1>css九宫格布局flex实现</h1>
             <div className='jiuGongGe'>
                 <div className='jiuGongGeChild'>
                     <div className='jiuGongGeItem'>1</div>
@@ -17,14 +17,14 @@ export default function CssLayoutPractice() {
                     <div className='jiuGongGeItem'>1</div>
                     <div className='jiuGongGeItem'>2</div>
                     <div className='jiuGongGeItem'>3</div>
-                </div>                
+                </div>
                 <div className='jiuGongGeChild'>
                     <div className='jiuGongGeItem'>1</div>
                     <div className='jiuGongGeItem'>2</div>
                     <div className='jiuGongGeItem'>3</div>
                 </div>
             </div>
-            <h2>css九宫格布局grid实现</h2>
+            <h1>css九宫格布局grid实现</h1>
             <div className="jiuGongGeByGrid">
                 <div className="jiuGongGeByGridItem">1</div>
                 <div className="jiuGongGeByGridItem">2</div>
@@ -36,10 +36,16 @@ export default function CssLayoutPractice() {
                 <div className="jiuGongGeByGridItem">8</div>
                 <div className="jiuGongGeByGridItem">9</div>
             </div>
-            <h2>左边固定右边自适应</h2>
+            <h1>左边固定右边自适应</h1>
             <div className='leftFixedRightAutofit'>
                 <div className='leftPart'></div>
                 <div className='rightPart'></div>
+            </div>
+            <h1>三列布局</h1>
+            <div className='sanlie'>
+                <div className='left'></div>
+                <div className="mid"></div>
+                <div className="right"></div>
             </div>
         </div>
     )
