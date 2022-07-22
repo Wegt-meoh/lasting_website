0228610abeeba03ec3ff976ac9730767cb6c7d87
lasting <lastingcoder@qq.com>
2022-07-22 13:10:53 +0800

    * 新增调用浏览器屏幕分享接口的组件 * 修复退出调用摄像头组件无法关闭摄像头视频流的问题

---
+++ src/components/Products/use-display-media/index.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useRef } from 'react'
+
+export default function Test() {
+    const video = useRef<HTMLVideoElement>(null)
+    const tracks = useRef<MediaStreamTrack[]>()
+
+    useEffect(() => {
+        navigator.mediaDevices.getDisplayMedia({
+            audio: true, video: true
+        }).then(value => {
+            tracks.current = value.getTracks()
+            if (video.current !== null) {
+                video.current.srcObject = value
+                video.current.play()
+            } else {
+                alert('video is null')
+                console.log('video is null')
+            }
+        }).catch(reason => {
+            alert(reason)
+            console.log(reason)
+        })
+
+        return () => {
+            tracks.current?.forEach(t => t.stop())
+        }
+    }, [])
+
+    return (
+        <video
+            style={{ zIndex: '100', maxWidth: '100%' }}
+            ref={video} />
+    )
+}
+
+export type AnyFunc = (...arg: any[]) => any
+
+function deboucing(callback: (...arg: any[]) => any, interval: number) {
+    let timer: NodeJS.Timeout | null = null
+    return () => {
+        if (timer !== null) clearTimeout(timer)
+        timer = setTimeout(callback, interval)
+    }
+}
+
+function throtting(callback: AnyFunc, interval: number, delay: number) {
+    let timer: NodeJS.Timeout | null = null
+    let startTime = Date.now()
+
+    return (...args: any[]) => {
+        const waitTime = Date.now() - startTime
+        if (timer !== null) clearTimeout(timer)
+        if (waitTime >= interval) {
+            callback.apply(args)
+            startTime = Date.now()
+        } else {
+            timer = setTimeout(callback, delay)
+        }
+    }
+}
--- src/components/Products/use-user-media/index.tsx
+++ src/components/Products/use-user-media/index.tsx
@@ -7,6 +7,7 @@ export default function UseUserMedia() {
     const videoRef = useRef<HTMLVideoElement>(null)
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const downloadRef = useRef<HTMLAnchorElement>(null)
+    const tracks = useRef<MediaStreamTrack[]>([])
 
     useEffect(() => {
         try {
@@ -15,6 +16,7 @@ export default function UseUserMedia() {
                     .then(mediaStream => {
                         if (videoRef.current !== null) {
                             videoRef.current.srcObject = mediaStream
+                            tracks.current = mediaStream.getTracks()
                             videoRef.current.play()
                         }
                     }).catch(reason => { alert('无法调用摄像头') })
@@ -24,54 +26,63 @@ export default function UseUserMedia() {
             console.log(`地址栏搜索chrome://flags/，找到Insecure origins treated as secure,把本站域名http://www.lastingcoder.xyz输入`)
         }
         return () => {
-            if (videoRef.current !== null) {
-                videoRef.current.srcObject = null
-            }
+            tracks.current.forEach(t => {
+                t.stop()
+            })
         }
     }, [])
 
     function saveImg() {
         if (canvasRef.current !== null && videoRef.current !== null) {
             canvasRef.current.getContext('2d')?.drawImage(videoRef.current, 0, 0)
-            const imgUrl = canvasRef.current.toDataURL()
-            const [typePart, dataPart] = imgUrl.split(',')
-            let bufferString = atob(dataPart)
-            let u8array = new Uint8Array(bufferString.length)
-            let n = bufferString.length
-            const mimeType = typePart.match(/:(.*);/)
-            while (n--) {
-                u8array[n] = bufferString.charCodeAt(n)
-            }
-            if (u8array !== null && mimeType !== null && downloadRef.current !== null) {
-                let blob = new Blob([u8array], { type: mimeType[1] })
-                let CompatibleURL = window.URL || window.webkitURL
-                const objUrl = CompatibleURL.createObjectURL(blob)
-                console.log(objUrl)
-                downloadRef.current.download = 'target.png'
-                downloadRef.current.href = objUrl
+            const canvascBlob=canvasRef.current.toBlob((blob):void=>{
+                if(blob===null||downloadRef.current===null) return
+                const url=URL.createObjectURL(blob)               
+                downloadRef.current.download='target.png'
+                downloadRef.current.href=url
                 downloadRef.current.click()
-                CompatibleURL.revokeObjectURL(objUrl)
-            }
+                URL.revokeObjectURL(url)
+            },undefined,1)
+            // const imgUrl = canvasRef.current.toDataURL()
+            // const [typePart, dataPart] = imgUrl.split(',')
+            // let bufferString = atob(dataPart)
+            // let u8array = new Uint8Array(bufferString.length)
+            // let n = bufferString.length
+            // const mimeType = typePart.match(/:(.*);/)
+            // while (n--) {
+            //     u8array[n] = bufferString.charCodeAt(n)
+            // }
+            // if (u8array !== null && mimeType !== null && downloadRef.current !== null) {
+            //     let blob = new Blob([u8array], { type: mimeType[1] })
+            //     let CompatibleURL = window.URL || window.webkitURL
+            //     const objUrl = CompatibleURL.createObjectURL(blob)
+            //     downloadRef.current.download = 'target.png'
+            //     downloadRef.current.href = objUrl
+            //     downloadRef.current.click()
+            //     CompatibleURL.revokeObjectURL(objUrl)
+            // }
         }
     }
 
     return (
-        <div className='useUserMedia'>
-            <div className='media-window'>
-                <video
-                    className={isMirror ? 'mirror' : ''}
+        <>
+            <div className='useUserMedia'>
+                <div className='media-window'>
+                    <video
+                        className={isMirror ? 'mirror' : ''}
+                        width={400}
+                        height={400}
+                        ref={videoRef} />
+                    <button onClick={() => { setIsMirror(!isMirror) }}>{isMirror ? '镜像' : '正常'}</button>
+                    <button onClick={saveImg}>截图</button>
+                </div>
+                <canvas
+                    className='useUserMidia-canvas'
                     width={400}
                     height={400}
-                    ref={videoRef} />
-                <button onClick={() => { setIsMirror(!isMirror) }}>{isMirror ? '镜像' : '正常'}</button>
-                <button onClick={saveImg}>截图</button>
+                    ref={canvasRef}></canvas>
+                <a hidden ref={downloadRef}>download</a>
             </div>
-            <canvas
-                className='useUserMidia-canvas'
-                width={400}
-                height={400}
-                ref={canvasRef}></canvas>
-            <a hidden ref={downloadRef}>download</a>
-        </div>
+        </>
     )
 }
